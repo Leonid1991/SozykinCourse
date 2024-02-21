0526c4d8a5145b643f14eefcdfe4943d7c26761a
Leo <23138125+Leonid1991@users.noreply.github.com>
2024-02-21 14:01:44 +0200

    Check the limit of the regression problem (Suzukin's course)

--- Regression.py
+++ Regression.py
@@ -16,13 +16,12 @@ x_train /= std
 x_test -= mean
 x_test /= std
 # Создаем нейронную сеть
-n_epoch = 10
+n_epoch = 100
 model = Sequential()
 model.add(Dense(128, activation='relu', input_shape=(x_train.shape[1],)))
 model.add(Dense(1))
 # Компилируем сеть
 model.compile(optimizer='adam', loss='mse', metrics=['mae'])
-print(model.summary())
 # Обучаем нейронную сеть
 history = model.fit(x_train, y_train, epochs=n_epoch, batch_size=1, verbose=2)
 # Оценка точности работы сети
